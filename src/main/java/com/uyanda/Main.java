651e7a4846f4a1a1deafe3f1b288f76eed54a68c
uyandam <uyandamphunga@gmail.com>
2020-08-31 19:43:05 +0200

    delete node

--- src/main/java/com/uyanda/Main.java
+++ src/main/java/com/uyanda/Main.java
@@ -13,14 +13,20 @@ public class Main {
     public static void main (String [] args){
         System.out.println("Hello World!");
         
-        BinarySearchTree node = new BinarySearchTree(10);
+        BinarySearchTree node = new BinarySearchTree();
+        node.add(10);
         node.add(5);
         node.add(15);
         node.add(3);
         node.add(7);
         node.add(13);
         node.add(18);
+        node.add(9);
         node.viewInorder();
-        
+        System.out.println(" ");
+        node.removeNode(5);
+        node.removeNode(3);
+        node.removeNode(18);
+        node.viewInorder();
     }
 }
--- src/main/java/com/uyanda/bst/BinarySearchTree.java
+++ src/main/java/com/uyanda/bst/BinarySearchTree.java
@@ -11,10 +11,12 @@ package com.uyanda.bst;
  */
 public class BinarySearchTree {
     
+    
+    /*
     public BinarySearchTree(int data){
         this.head = new Node(data);
     }
-    
+    */
     
     public void add(int data){
         this.head = insertRecord(this.head, data);
@@ -23,6 +25,47 @@ public class BinarySearchTree {
     public void viewInorder(){
         Node current = this.head;
         printInorder(current);
+    }
+    
+    public void removeNode(int data){
+        Node current = this.head;
+        
+        this.head = deleteNode(current, data);
+    }
+    
+    private Node deleteNode(Node node, int data){
+        
+        if(node == null){
+            node = null;
+        }else if(data < node.data){
+            node.left = deleteNode(node.left, data);
+        }else if(data > node.data){
+            node.right = deleteNode(node.right, data);
+        } else{
+            if(node.left == null && node.right == null){
+                node = null;
+            } else if(node.left == null ){
+                node = node.right;
+            } else if(node.right == null){
+                node = node.left;
+            } else{
+                int minValue = findMin(node.right);
+                node.data = minValue;
+                node.right = deleteNode(node.right, minValue);
+            }
+        }
+        return node;
+        
+    }
+    
+    private int findMin(Node node){
+        Node current = node;
+        
+        while(current.left != null){
+            current = current.left;
+        }
+        
+        return current.data;
     }
     
     private void printInorder(Node node){
